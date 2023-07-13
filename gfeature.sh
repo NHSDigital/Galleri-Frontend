11122915b96c467db39d79884aacf5317fcf60ae
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-07-13 16:25:27 +0100

    updated

--- gfeature.sh
+++ gfeature.sh
@@ -2,4 +2,4 @@ git checkout -b "gal-$1/$2" develop
 currBranch= git rev-parse --symbolic-full-name --abbrev-ref HEAD
 echo "Created: $currBranch"
 git push $currBranch
-echo "Pushing: $currBranch"
+echo "Pushed initial:$currBranch"
