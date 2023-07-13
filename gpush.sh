b4476b6da565f81093ac0892c424ac1c2d508f19
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-07-13 16:25:27 +0100

    updated

--- gpush.sh
+++ gpush.sh
@@ -2,4 +2,4 @@ git add .
 git commit -m "$1"
 currBranch= git rev-parse --symbolic-full-name --abbrev-ref HEAD
 git push $currBranch
-echo "Pushed: $currBranch \nMessage: $1"
+echo "Pushed:$currBranch \nMessage:$1"
