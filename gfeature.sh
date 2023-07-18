402e6e46bf2a7f22bb2944ba01274b48bdbedc00
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-07-18 11:02:59 +0100

    removed bash files

--- gfeature.sh
+++
@@ -1,5 +0,0 @@
-git checkout -b "gal-$1/$2" develop
-currBranch= git rev-parse --symbolic-full-name --abbrev-ref HEAD
-echo "Created: $currBranch"
-git push $currBranch
-echo "Pushed initial: " + $currBranch
--- gpush.sh
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "$1"
-currBranch= git rev-parse --symbolic-full-name --abbrev-ref HEAD
-git push $currBranch
-echo "Message: $1"
