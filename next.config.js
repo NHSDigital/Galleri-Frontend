c50cebae4eacf0e40cd2aa859a80d6312f7c27d9
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-08-02 21:13:11 +0100

    Added default NHS Galleri template page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename)
+
+const nextConfig = {
+  // sassOptions: {
+  //   includePath: [path.join(__dirname, 'styles')],
+  // },
 };
+
 export default nextConfig;
---
+++ src/app/components/content.js
@@ -0,0 +1,11 @@
+import '../styles/css/sass.css'
+
+export default function Content() {
+  return (
+    <div class="nhsuk-width-container ">
+      <main class="nhsuk-main-wrapper " id="maincontent" role="main">
+        <h1>Default page template</h1>
+      </main>
+    </div>
+  )
+}
