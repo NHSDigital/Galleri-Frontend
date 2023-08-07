e8fdbbf933b33aabdca17b872a2d67a4b6355b22
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-08-07 15:53:01 +0100

    removed comments

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename)
 
 const nextConfig = {
   // sassOptions: {
