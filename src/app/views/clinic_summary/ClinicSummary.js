47b7e244718dfa5cfe3d69d2e9546a30b8e39e69
Zain Malik <zain.malik3@nhs.net>
2023-11-27 14:09:17 +0000

    api id change

--- src/app/views/clinic_summary/ClinicSummary.js
+++ src/app/views/clinic_summary/ClinicSummary.js
@@ -35,7 +35,7 @@ export default class ClinicSummary extends Component {
     // TODO:Replace api id with latest api id from aws console until we get custom domain name set up
     axios
       .get(
-        `https://kdjzyje3j0.execute-api.eu-west-2.amazonaws.com/dev/clinic-summary-list?participatingIcb=${this.context.state.icbSelected}`
+        `https://z5ks8p0buh.execute-api.eu-west-2.amazonaws.com/dev/clinic-summary-list?participatingIcb=${this.context.state.icbSelected}`
       )
       .then((response) => {
         this.context.setState({
@@ -93,7 +93,7 @@ export default class ClinicSummary extends Component {
       axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
       axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
       // TODO:Replace api id with latest api id from aws console until we get custom domain name set up
-      const response = await axios.get(`https://n9plubaxbj.execute-api.eu-west-2.amazonaws.com/dev/participating-icb-list`);
+      const response = await axios.get(`https://mxtf0w9wq9.execute-api.eu-west-2.amazonaws.com/dev/participating-icb-list`);
 
       // Update the state
       this.context.setState({
