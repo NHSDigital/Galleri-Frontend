b6709126ab6c23f1f9f37a4612e3d3002b31311b
Zain Malik <zain.malik3@nhs.net>
2023-11-28 09:25:33 +0000

    linitnig changes

--- src/app/views/invitation_summary/SummaryListFirst.jsx
+++ src/app/views/invitation_summary/SummaryListFirst.jsx
@@ -9,70 +9,70 @@ export default function SummaryListFirst({
   totalToInvite
   })
 
- {
-  const { appsRemaining } = props;
+  {
+    const { appsRemaining } = props;
 
-  return (
-    <dl id="summary-list-1" class="nhsuk-summary-list nhsuk-u-margin-bottom-9">
-      <div class="nhsuk-summary-list__row">
-        <dt
-          style={{ width: "350px" }}
-          id="term1-label"
-          class="nhsuk-summary-list__key"
-        >
-          Distance from clinic
-        </dt>
-        <dd
-          id="term1-value"
-          class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-        >
-          + {rangeSelection} miles
-        </dd>
-      </div>
-      <div class="nhsuk-summary-list__row">
-        <dt id="term2-label" class="nhsuk-summary-list__key">
-          Total available to invite
-        </dt>
-        <dd
-          id="term2-value"
-          class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-        >
-          {totalToInvite}
-        </dd>
-      </div>
-      <div class="nhsuk-summary-list__row">
-        <dt id="term3-label" class="nhsuk-summary-list__key">
-          Appointments remaining
-        </dt>
-        <dd
-          id="term3-value"
-          class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-        >
-          {appsRemaining}
-        </dd>
-      </div>
-      <div class="nhsuk-summary-list__row">
-        <dt id="term4-label" class="nhsuk-summary-list__key">
-          Target percentage of appointments to fill
-        </dt>
-        <dd
-          id="term4-value"
-          class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-        >
-          {targetPercentageToFill}%
-        </dd>
-      </div>
-      <div class="nhsuk-summary-list__row">
-        <dt id="term5-label" class="nhsuk-summary-list__key">
-          Target number of appointments to fill
-        </dt>
-        <dd
-          id="term5-value"
-          class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-        >
-          {targetAppToFill}
-        </dd>
-      </div>
-    </dl>
-  );
+    return (
+      <dl id="summary-list-1" class="nhsuk-summary-list nhsuk-u-margin-bottom-9">
+        <div class="nhsuk-summary-list__row">
+          <dt
+            style={{ width: "350px" }}
+            id="term1-label"
+            class="nhsuk-summary-list__key"
+          >
+            Distance from clinic
+          </dt>
+          <dd
+            id="term1-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            + {rangeSelection} miles
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term2-label" class="nhsuk-summary-list__key">
+            Total available to invite
+          </dt>
+          <dd
+            id="term2-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            {totalToInvite}
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term3-label" class="nhsuk-summary-list__key">
+            Appointments remaining
+          </dt>
+          <dd
+            id="term3-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            {appsRemaining}
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term4-label" class="nhsuk-summary-list__key">
+            Target percentage of appointments to fill
+          </dt>
+          <dd
+            id="term4-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            {targetPercentageToFill}%
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term5-label" class="nhsuk-summary-list__key">
+            Target number of appointments to fill
+          </dt>
+          <dd
+            id="term5-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            {targetAppToFill}
+          </dd>
+        </div>
+      </dl>
+    );
 }
