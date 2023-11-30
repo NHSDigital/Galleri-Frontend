83c3614d1588119a740a4ca62c8675a0da612820
Zain Malik <zain.malik3@nhs.net>
2023-11-30 13:52:53 +0000

    add styling and restructure

--- src/app/views/invitation_summary/SummaryListFirst.jsx
+++ src/app/views/invitation_summary/SummaryListFirst.jsx
@@ -14,10 +14,42 @@ export default function SummaryListFirst({
     return (
       <dl id="summary-list-1" class="nhsuk-summary-list nhsuk-u-margin-bottom-9">
         <div class="nhsuk-summary-list__row">
+          <dt id="term3-label" class="nhsuk-summary-list__key nhsuk-summary-list-custom__key">
+            Appointments remaining
+          </dt>
+          <dd
+            id="term3-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
+          >
+            {appsRemaining}
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term4-label" class="nhsuk-summary-list__key nhsuk-summary-list-custom__key">
+            Target percentage of appointments to fill
+          </dt>
+          <dd
+            id="term4-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4 nhsuk-summary-list__value_custom__value"
+          >
+            {targetPercentageToFill}%
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
+          <dt id="term5-label" class="nhsuk-summary-list__key nhsuk-summary-list-custom__key">
+            Target number of appointments to fill
+          </dt>
+          <dd
+            id="term5-value"
+            class="nhsuk-summary-list__value nhsuk-u-padding-left-4 nhsuk-summary-list__value_custom__value"
+          >
+            {targetAppToFill}
+          </dd>
+        </div>
+        <div class="nhsuk-summary-list__row">
           <dt
-            style={{ width: "350px" }}
             id="term1-label"
-            class="nhsuk-summary-list__key"
+            class="nhsuk-summary-list__key nhsuk-summary-list-custom__key"
           >
             Distance from clinic
           </dt>
@@ -29,7 +61,7 @@ export default function SummaryListFirst({
           </dd>
         </div>
         <div class="nhsuk-summary-list__row">
-          <dt id="term2-label" class="nhsuk-summary-list__key">
+          <dt id="term2-label" class="nhsuk-summary-list__key nhsuk-summary-list-custom__key">
             Total available to invite
           </dt>
           <dd
@@ -37,39 +69,6 @@ export default function SummaryListFirst({
             class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
           >
             {totalToInvite}
-          </dd>
-        </div>
-        <div class="nhsuk-summary-list__row">
-          <dt id="term3-label" class="nhsuk-summary-list__key">
-            Appointments remaining
-          </dt>
-          <dd
-            id="term3-value"
-            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-          >
-            {appsRemaining}
-          </dd>
-        </div>
-        <div class="nhsuk-summary-list__row">
-          <dt id="term4-label" class="nhsuk-summary-list__key">
-            Target percentage of appointments to fill
-          </dt>
-          <dd
-            id="term4-value"
-            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-          >
-            {targetPercentageToFill}%
-          </dd>
-        </div>
-        <div class="nhsuk-summary-list__row">
-          <dt id="term5-label" class="nhsuk-summary-list__key">
-            Target number of appointments to fill
-          </dt>
-          <dd
-            id="term5-value"
-            class="nhsuk-summary-list__value nhsuk-u-padding-left-4"
-          >
-            {targetAppToFill}
           </dd>
         </div>
       </dl>
