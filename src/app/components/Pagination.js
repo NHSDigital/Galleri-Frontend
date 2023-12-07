51aa0cd62456d54df5967498915d163be156739a
Zain <zain.malik3@nhs.net>
2023-12-07 15:02:48 +0000

    revert

--- src/app/components/Pagination.js
+++ src/app/components/Pagination.js
@@ -77,15 +77,13 @@ const Pagination = props => {
 
   if (currentPage === 0) {
     return null;
-  } 
+  }
   else if (paginationRange.length < 2) {
     return (
       <nav class="nhsuk-pagination style_pagination__7B72Z" aria-label="Pagination Navigation">
         <div class="style_summary__J3_1h">
           <span index="0" node="[object Object]">
-            {`Showing ${currentPage * pageSize - pageSize +
-              1}-${ currentPage * pageSize < totalCount ? currentPage * pageSize
-                : totalCount} of ${totalCount} results`}
+            {`Showing ${currentPage * pageSize - pageSize + 1}-${ currentPage * pageSize < totalCount ? currentPage * pageSize : totalCount} of ${totalCount} results`}
           </span>
         </div>
       </nav>
@@ -94,25 +92,22 @@ const Pagination = props => {
   let lastPage = paginationRange[paginationRange.length - 1];
 
   const onNext = () => {
-    if (currentPage !== lastPage) 
+    if (currentPage !== lastPage)
       onPageChange(currentPage + 1);
   };
 
   const onPrevious = () => {
-    if (currentPage !== 1) 
+    if (currentPage !== 1)
       onPageChange(currentPage - 1);
   };
 
   return (
     <nav
-      class="nhsuk-pagination style_pagination__7B72Z"
-      role="navigation" aria-label="Pagination Navigation"
+      class="nhsuk-pagination style_pagination__7B72Z" role="navigation" aria-label="Pagination Navigation"
     >
       <div class="style_summary__J3_1h" role="status" aria-live="polite">
         <span index="0" node="[object Object]">
-          {`Showing ${currentPage * pageSize - pageSize +
-            1}-${ currentPage * pageSize < totalCount ? currentPage * pageSize
-              : totalCount} of ${totalCount} results`}
+          {`Showing ${currentPage * pageSize - pageSize + 1}-${ currentPage * pageSize < totalCount ? currentPage * pageSize : totalCount} of ${totalCount} results`}
         </span>
       </div>
       <h2 class="nhsuk-u-visually-hidden">Pagination Support links</h2>
@@ -120,8 +115,7 @@ const Pagination = props => {
         <li class="style_item__Y9BLA" id="prevButton">
           <a
             aria-label="Previous page: « Previous"
-            class={`nhsuk-pagination__link style_link__ToZGL 
-            ${currentPage == 1 ? "style_current__K8c2u" : ""} `}
+            class={`nhsuk-pagination__link style_link__ToZGL ${currentPage == 1 ? "style_current__K8c2u" : ""} `}
             href="#"
             onClick={onPrevious}
           >
@@ -134,14 +128,13 @@ const Pagination = props => {
           }
 
           return (
-            <li key={pgNumber} 
-              class="style_item__Y9BLA">
-              <a
-                onClick={() => onPageChange(pgNumber)}
+            <li key={pgNumber}
+              class="style_item__Y9BLA"
+            >
+              <a onClick={() => onPageChange(pgNumber)}
                 href="#"
                 aria-label={`Go to Page ${pgNumber}`}
-                class={`style_link__ToZGL ${currentPage == 
-                  pgNumber ? "style_current__K8c2u" : ""} `}
+                class={`style_link__ToZGL ${currentPage == pgNumber ? "style_current__K8c2u" : ""} `}
               >
                 {pgNumber}
               </a>
@@ -152,8 +145,7 @@ const Pagination = props => {
           <a
             aria-label="Next page: Next »"
             href="#"
-            class={`nhsuk-pagination__link style_link__ToZGL 
-            ${currentPage == lastPage ? "style_current__K8c2u" : ""} `}
+            class={`nhsuk-pagination__link style_link__ToZGL ${currentPage == lastPage ? "style_current__K8c2u" : ""} `}
             onClick={onNext}
           >
             Next »
