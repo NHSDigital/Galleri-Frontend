599793fdbd201e7bba04ec023002b891f3b4bcf9
Neerja Goyal <neerjagoyal@Neerjas-MacBook-Pro.local>
2023-12-12 08:36:35 +0000

    test cases added

--- src/app/views/invitation_planning/InvitationPlannin.test.js
+++ src/app/views/invitation_planning/InvitationPlannin.test.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { sumQuintiles } from "./helper";
 import InvitationPlanningPage from "./InvitationPlanningPage";
+import InvitationPlanning from "./InvitationPlanning";
+import QuintileTargetTable from "./QuintileTargetTable";
+
+window.HTMLElement.prototype.scrollIntoView = function() {};
 
 describe("Clinic Summary helper tests", () => {
   test("sumQuitiles returns correct sum", () => {
@@ -17,29 +21,31 @@ describe("Clinic Summary helper tests", () => {
   });
 });
 
+const invitationPlanning = (
+  <InvitationPlanningPage
+    quintileValues={{}}
+    quintileValuesAux={{}}
+    enableFillEdit={false}
+    lastUpdatedQuintile={"lastUpdated"}
+    userName={"lastUpdated"}
+    isCorrectTotal={true}
+    nationalUptakePercentage={100}
+    enableUptakeEdit={true}
+    isCorrectUptakeTotal={true}
+    onQuintileChangeHandler={() => {}}
+    onAmendFillHandler={() => {}}
+    onCancelSaveFillHandler={() => {}}
+    onAmendForecastHandler={() => {}}
+    onUptakeChangeHandler={() => {}}
+    onCancelSaveForecastHandler={() => {}}
+    onSaveForecastHandler={() => {}}
+    onSaveFillHandler={() => {}}
+    sumQuintiles={() => {}}
+    onKeyUp={() => {}}
+  />
+);
+
 describe("Invitation planning render", () => {
-  const invitationPlanning = (
-    <InvitationPlanningPage
-      quintileValues={{}}
-      quintileValuesAux={{}}
-      enableFillEdit={false}
-      lastUpdatedQuintile={"lastUpdated"}
-      userName={"lastUpdated"}
-      isCorrectTotal={true}
-      nationalUptakePercentage={100}
-      enableUptakeEdit={false}
-      isCorrectUptakeTotal={true}
-      onQuintileChangeHandler={() => {}}
-      onAmendFillHandler={() => {}}
-      onCancelSaveFillHandler={() => {}}
-      onAmendForecastHandler={() => {}}
-      onUptakeChangeHandler={() => {}}
-      onCancelSaveForecastHandler={() => {}}
-      onSaveForecastHandler={() => {}}
-      onSaveFillHandler={() => {}}
-      sumQuintiles={() => {}}
-    />
-  );
 
   test("Page header renders", () => {
     render(invitationPlanning);
@@ -67,5 +73,45 @@ describe("Invitation planning render", () => {
     render(invitationPlanning);
     let label = screen.getByText("Quintile fill target");
     expect(label.innerHTML).toBe("Quintile fill target<br>");
+  });
+});
+
+describe("Error Scenarios", () => {
+  test("National Forecast Uptake error when forecast value is not between 1 and 100", () => {
+    const { container } = render(<InvitationPlanning />);
+    const button = screen.getByText('Amend forecast uptake');
+    expect(button.innerHTML).toBe('Amend forecast uptake');
+    
+    fireEvent.click(button);
+    const myInputElement = container.querySelector(`input[id="national-forecast-uptake"]`);
+    expect(myInputElement).toBeInTheDocument();
+
+    fireEvent.change(myInputElement, {target: {value: '230'}});
+    let savebutton = screen.getByText("Save changes");
+    expect(savebutton.innerHTML).toBe("Save changes");
+
+    fireEvent.click(savebutton);
+    
+    let error = screen.getAllByText("The uptake percentage must be between 1% and 100%");
+    expect(error[0].innerHTML).toBe('The uptake percentage must be between 1% and 100%');
+  });
+
+  const quintileTable = (
+    <QuintileTargetTable
+        quintileValues={{}}
+        quintileValuesAux={{}}
+        enableFillEdit={true}
+        isCorrectTotal={false}
+        onQuintileChangeHandler={() => {}}
+        sumQuintiles={() => {}}
+    />
+  );
+
+  test("Quintile fill target error when values dont add to 100", () => {
+    
+
+    render(quintileTable);
+    let error = screen.getAllByText("The fill targets must add up to 100%");
+    expect(error[0].innerHTML).toBe('The fill targets must add up to 100%');
   });
 });
