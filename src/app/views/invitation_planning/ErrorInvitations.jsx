4e3427e19665cd7836e1f86c1fe3b412fe666b0e
NeerjaGoyalNHS <153278535+NeerjaGoyalNHS@users.noreply.github.com>
2023-12-13 11:33:30 +0000

    Update ErrorInvitations.jsx
    Changes for CI/CD Pipeline / check-file-format / check-file-format (pull_request)

--- src/app/views/invitation_planning/ErrorInvitations.jsx
+++ src/app/views/invitation_planning/ErrorInvitations.jsx
@@ -7,7 +7,6 @@ export default function Errorinvitations(props) {
         isCorrectTotal,
         isCorrectUptakeTotal,
   } = props;
-  
   return (
     <div
       data-testid="error-banner"
@@ -23,7 +22,7 @@ export default function Errorinvitations(props) {
       >
         There is a problem
       </h2>
-      <div class="nhsuk-error-summary__body">    
+      <div class="nhsuk-error-summary__body"> 
             {!isCorrectUptakeTotal && (
                 <a href="#uptake-error-message">
                     The uptake percentage must be between 1% and 100%
