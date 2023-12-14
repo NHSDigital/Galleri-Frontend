69c96e50dbf96fae30fa883f3cd96d8f9225b9e2
Amar-NHS <amar.rai5@nhs.net>
2023-12-14 11:37:39 +0000

    gal-703# autosignout modification

--- src/app/context/AutoSignOutProvider.jsx
+++ src/app/context/AutoSignOutProvider.jsx
@@ -1,19 +1,26 @@
 "use client";
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useSession } from 'next-auth/react';
 
 const timeout = 60000 ; // 10 minute in milliseconds
 
 const AutoSignOutProvider = ({ children }) => {
   const [timer, setTimer] = useState(null);
+  const [remainingTime, setRemainingTime] = useState(timeout / 1000);
   const { data: session } = useSession();
+  const resetTimerRef = useRef();
 
   // Function to reset the timer
   const resetTimer = useCallback(() => {
+    console.log('Resetting timer function.');
     clearTimeout(timer);
     // Set a new timer when called
     setTimer(setTimeout(() => handleSignOut(), timeout));
-  }, [timer]);
+    setRemainingTime(timeout / 1000);
+  }, [timer, remainingTime]);
+
+  // Save the current resetTimer function to the ref
+  resetTimerRef.current = resetTimer;
 
   // Function to handle the sign-out action
   const handleSignOut = async () => {
@@ -28,12 +35,13 @@ const AutoSignOutProvider = ({ children }) => {
     return () => {
       clearTimeout(timer);
     };
-  }, []); // Empty dependency array ensures the effect runs only on mount and unmount
+  }, [remainingTime]); // Empty dependency array ensures the effect runs only on mount and unmount
 
   useEffect(() => {
     // Function to reset the timer on user activity
     const resetTimerOnActivity = () => {
-      resetTimer();
+      console.log('User activity detected! Resetting timer.');
+      resetTimerRef.current();
     };
 
     // Attach event listeners for user activity
@@ -47,14 +55,36 @@ const AutoSignOutProvider = ({ children }) => {
       document.removeEventListener('click', resetTimerOnActivity);
       document.removeEventListener('touchstart', resetTimerOnActivity);
     };
-  }, [resetTimer]);
+  }, []);
 
   useEffect(() => {
     // Reset the timer when the session changes
-    resetTimer();
-  }, [session, resetTimer]);
+    resetTimerRef.current();
+  }, [session]);
 
-  return <>{children}</>;
+  useEffect(() => {
+    // Update remaining time every second
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => Math.max(0, prevTime - 1));
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    // Check if remaining time is zero and trigger sign-out
+    if (remainingTime === 0) {
+      handleSignOut();
+    }
+  }, [remainingTime]);
+
+  return (
+    <>
+      <div>Remaining Time: {remainingTime} seconds</div>
+      {children}
+    </>
+  );
 };
 
 export default AutoSignOutProvider;
