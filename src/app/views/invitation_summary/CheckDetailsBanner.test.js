f292d395d03e1d9968e9779f33caca5ef1522740
NeerjaGoyalNHS <153278535+NeerjaGoyalNHS@users.noreply.github.com>
2023-12-22 15:39:56 +0000

    Update CheckDetailsBanner.test.js
    Pipeline changes

--- src/app/views/invitation_summary/CheckDetailsBanner.test.js
+++ src/app/views/invitation_summary/CheckDetailsBanner.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import "@testing-library/jest-dom";
-import CheckDetailsBanner from './checkDetailsBanner';
+import CheckDetailsBanner from './CheckDetailsBanner';
 
 describe('CheckDetailsBanner Component', () => {
   const mockProps = {
