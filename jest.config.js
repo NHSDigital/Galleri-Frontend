0fb3aef5465ad93dd24f105053ec21b1d489bf2b
Amar-NHS <amar.rai5@nhs.net>
2024-01-03 09:56:13 +0000

    gal-775# changed jest config file to exclude the test files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-import nextJest from 'next/jest.js'
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
@@ -11,8 +11,17 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
-  testEnvironment: 'jest-environment-jsdom',
-}
+  testEnvironment: "jest-environment-jsdom",
+
+  collectCoverage: true,
+
+  // Exclude coverage reports from test files and node_modules
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files
+    "!**/*.test.{js,jsx,ts,tsx}", // Exclude test files
+    "!**/node_modules/**",
+  ],
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
