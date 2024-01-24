6a8bd5ab236b611a84f727c8812967f35ab3d111
avi312NHS <aviraj.mandair1@nhs.net>
2024-01-24 15:25:40 +0000

    linting with prettier

--- src/app/views/logged_out/LoggedOut.test.jsx
+++ src/app/views/logged_out/LoggedOut.test.jsx
@@ -1,39 +1,32 @@
-import React from "react";
-import "@testing-library/jest-dom";
-import {
-  render,
-  screen,
-  fireEvent,
-  act,
-  waitFor,
-} from "@testing-library/react";
-import { InactivityProvider } from "../../context/AutoSignOutProvider.jsx";
-import LoggedOut from "./LoggedOut";
-import Root from "../../page";
-import PrivacyConfirmationPage from "../privacy_confirmation/PrivacyConfirmationPage";
-import { useSession } from "next-auth/react";
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { InactivityProvider } from '../../context/AutoSignOutProvider.jsx';
+import LoggedOut from './LoggedOut';
+import Root from '../../page';
+import { useSession } from 'next-auth/react';
 
-jest.mock("../privacy_confirmation/PrivacyConfirmationPage", () => {
+jest.mock('../privacy_confirmation/PrivacyConfirmationPage', () => {
   return {
     __esModule: true,
     default: () => (
-      <div data-testid="privacy-confirmation-page">
+      <div data-testid='privacy-confirmation-page'>
         Privacy Confirmation Start Page
       </div>
     ),
   };
 });
 
-jest.mock("next-auth/react", () => ({
-  ...jest.requireActual("next-auth/react"),
+jest.mock('next-auth/react', () => ({
+  ...jest.requireActual('next-auth/react'),
   useSession: jest.fn(),
 }));
 
-describe("LoggedOut page", () => {
-  test("it should render logged out page", () => {
+describe('LoggedOut page', () => {
+  test('it should render logged out page', () => {
     const mockSession = {
-      user: { name: "Test User", email: "test@example.com" },
-      expires: "mock-expiry",
+      user: { name: 'Test User', email: 'test@example.com' },
+      expires: 'mock-expiry',
     };
 
     useSession.mockReturnValueOnce([null, true]);
@@ -44,17 +37,17 @@ describe("LoggedOut page", () => {
         <LoggedOut />
       </InactivityProvider>
     );
-    const logoutButton = screen.getByLabelText("Continue");
-    const logoutHeader = screen.getByTestId("log-out-header");
+    const logoutButton = screen.getByLabelText('Continue');
+    const logoutHeader = screen.getByTestId('log-out-header');
 
     expect(logoutButton).toBeDefined();
     expect(logoutHeader).toBeDefined();
   });
 
-  test("it shows continue button after timeout", async () => {
+  test('it shows continue button after timeout', async () => {
     const mockSession = {
-      user: { name: "Test User", email: "test@example.com" },
-      expires: "mock-expiry",
+      user: { name: 'Test User', email: 'test@example.com' },
+      expires: 'mock-expiry',
     };
 
     useSession.mockReturnValueOnce([null, true]);
@@ -66,14 +59,14 @@ describe("LoggedOut page", () => {
       </InactivityProvider>
     );
 
-    expect(queryByTestId("privacy-confirmation-page")).toBeInTheDocument();
-    const logoutButton = getByLabelText("Continue");
+    expect(queryByTestId('privacy-confirmation-page')).toBeInTheDocument();
+    const logoutButton = getByLabelText('Continue');
     await waitFor(() => {
       expect(logoutButton).toBeInTheDocument();
     }, 1100);
 
     fireEvent.click(logoutButton);
-    expect(queryByTestId("privacy-confirmation-page")).toBeInTheDocument();
+    expect(queryByTestId('privacy-confirmation-page')).toBeInTheDocument();
     expect(logoutButton).toBeDefined();
   });
 });
--- src/app/views/privacy_confirmation/PrivacyConfirmation.test.js
+++ src/app/views/privacy_confirmation/PrivacyConfirmation.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "@testing-library/jest-dom";
-import { render, screen, fireEvent, act } from "@testing-library/react";
-import PrivacyConfirmation from "./PrivacyConfirmation";
-import { useInactivity } from "../../context/AutoSignOutProvider.jsx";
-import { useSession } from "next-auth/react";
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import PrivacyConfirmation from './PrivacyConfirmation';
+import { useInactivity } from '../../context/AutoSignOutProvider.jsx';
+import { useSession } from 'next-auth/react';
 
-jest.mock("next-auth/react");
+jest.mock('next-auth/react');
 
-jest.mock("../../context/AutoSignOutProvider");
+jest.mock('../../context/AutoSignOutProvider');
 
 const mockInactivityValues = {
   showLogoutPage: false,
@@ -15,8 +15,8 @@ const mockInactivityValues = {
 };
 
 const mockSession = {
-  user: { name: "Test User", email: "test@example.com" },
-  expires: "mock-expiry",
+  user: { name: 'Test User', email: 'test@example.com' },
+  expires: 'mock-expiry',
 };
 
 useSession.mockReturnValue([mockSession, false]);
@@ -29,51 +29,51 @@ global.renderWithInactivityProvider = (ui, options) => {
   );
 };
 
-describe("ClinicInfo", () => {
+describe('ClinicInfo', () => {
   const mockProps = {};
 
-  jest.mock("next-auth/react", () => ({
-    ...jest.requireActual("next-auth/react"),
+  jest.mock('next-auth/react', () => ({
+    ...jest.requireActual('next-auth/react'),
     useSession: jest.fn(),
   }));
 
-  test("renders PrivacyConfirmation correctly", () => {
+  test('renders PrivacyConfirmation correctly', () => {
     render(<PrivacyConfirmation {...mockProps} />);
-    expect(screen.getByText("Protecting patient data")).toBeInTheDocument();
+    expect(screen.getByText('Protecting patient data')).toBeInTheDocument();
     expect(
       screen.getByText(
-        "You must only access a participant’s records if there is a legitimate requirement to access their data."
+        'You must only access a participant’s records if there is a legitimate requirement to access their data.'
       )
     ).toBeInTheDocument();
     expect(
       screen.getByText(
-        "All activity in the Galleri pilot system is routinely checked to ensure records are not accessed inappropriately."
+        'All activity in the Galleri pilot system is routinely checked to ensure records are not accessed inappropriately.'
       )
     ).toBeInTheDocument();
     expect(
-      screen.getByText("When you are finished using the system you must:")
+      screen.getByText('When you are finished using the system you must:')
     ).toBeInTheDocument();
     expect(
-      screen.getByText("Log out using the button in the top right hand corner.")
+      screen.getByText('Log out using the button in the top right hand corner.')
     ).toBeInTheDocument();
-    expect(screen.getByText("Remove your smart card.")).toBeInTheDocument();
+    expect(screen.getByText('Remove your smart card.')).toBeInTheDocument();
     expect(
-      screen.getByText("I have read and understood this message")
+      screen.getByText('I have read and understood this message')
     ).toBeInTheDocument();
   });
 
-  test("renders PrivacyConfirmation errors correctly", () => {
+  test('renders PrivacyConfirmation errors correctly', () => {
     render(<PrivacyConfirmation {...mockProps} />);
-    const checkbox = screen.getByTestId("confirm-privacy");
+    const checkbox = screen.getByTestId('confirm-privacy');
     expect(checkbox.checked).toEqual(false);
     // Simulate a click on the "Continue" button while checkbox unchecked
-    fireEvent.click(screen.getByText("Continue"));
-    expect(screen.getByText("There is a problem")).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Continue'));
+    expect(screen.getByText('There is a problem')).toBeInTheDocument();
   });
 
-  test("renders PrivacyConfirmation checkbox to function correctly", () => {
+  test('renders PrivacyConfirmation checkbox to function correctly', () => {
     render(<PrivacyConfirmation {...mockProps} />);
-    const checkbox = screen.getByTestId("confirm-privacy");
+    const checkbox = screen.getByTestId('confirm-privacy');
     expect(checkbox.checked).toEqual(false);
     fireEvent.click(checkbox);
     expect(checkbox.checked).toEqual(true);
