9dc56f996c3847ffb8e31e38837ca5672a7877b9
Amar-NHS <amar.rai5@nhs.net>
2024-01-29 14:50:42 +0000

    hotfix/added url catcher since hostname starts chaning on Elastic Beanstalk

--- src/app/components/NavMenu.jsx
+++ src/app/components/NavMenu.jsx
@@ -1,10 +1,18 @@
 "use client";
 import Link from "next/link";
 import "../styles/css/sass.css";
+import { useEffect, useState } from "react";
 import { signOut } from "next-auth/react";
 
 // Navigation Menu
 export default function NavMenu() {
+  const [currentUrl, setCurrentUrl] = useState("");
+  useEffect(() => {
+    // Get the current page URL
+    const currentUrl = window.location.href;
+    setCurrentUrl(currentUrl);
+    console.log("Current URL:", currentUrl);
+  }, []);
   return (
     <nav
       className="nhsuk-navigation"
@@ -33,7 +41,10 @@ export default function NavMenu() {
           <a
             class="nhsuk-header__navigation-link"
             style={{ cursor: "pointer" }}
-            onClick={() => signOut()}
+            onClick={() => {
+              console.log(currentUrl);
+              signOut({ callbackUrl: `${currentUrl}signin` });
+            }}
             onKeyDown={(e) => {
               if (e.key === "Enter" || e.key === "Space") {
                 // Handle the keyboard interaction, e.g., trigger the same action as onClick
