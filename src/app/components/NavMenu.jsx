94b8c7f42a71f7b003c6aeff64234113c64a8593
Amar-NHS <amar.rai5@nhs.net>
2024-01-29 10:20:59 +0000

    hotfix/ammended signout callbackUrl

--- src/app/components/NavMenu.jsx
+++ src/app/components/NavMenu.jsx
@@ -33,7 +33,7 @@ export default function NavMenu() {
           <a
             class="nhsuk-header__navigation-link"
             style={{ cursor: "pointer" }}
-            onClick={() => signOut({ callbackUrl: "/signin" })}
+            onClick={() => signOut()}
             onKeyDown={(e) => {
               if (e.key === "Enter" || e.key === "Space") {
                 // Handle the keyboard interaction, e.g., trigger the same action as onClick
