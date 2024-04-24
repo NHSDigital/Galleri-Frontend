b94aa9176405dd3140454a850e769c90143f23bb
Abdulqadir Qasim <abdulqadir.qasim1@nhs.net>
2024-04-24 15:32:58 +0100

    Fixed logic issues

--- scripts/pipeline/version.sh
+++ scripts/pipeline/version.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+NEW_VERSION="0.1.0"
 REPO_TAGS=$(git tag)
 SKIP_VERSION_BUMP=false
 TAG_MESSAGE="New PR on $(date)"
@@ -10,22 +11,21 @@ VERSION_TO_BUMP=$1
 
 if [[ -z $REPO_TAGS ]]; then
     echo "No tags detected in repo. Adding initial tag '0.1.0'"
-    NEW_VERSION="0.1.0"
     SKIP_VERSION_BUMP=true
 fi
 
-if [[ ("$SKIP_VERSION_BUMP" = false) && (-z $VERSION) ]]; then
+if [[ ("$SKIP_VERSION_BUMP" == false) && (-z $VERSION) ]]; then
     echo "Error: Unable to find previous Github Tag. Ensure at least the current closest tags is annotated."
     exit 1
 fi
 
-if [ $SKIP_VERSION_BUMP == false ]; then
+if [[ "$SKIP_VERSION_BUMP" == false ]]; then
     set - $(echo $VERSION |sed 's/\./ /g')
-    if [ $VERSION_TO_BUMP == "MINOR" ]; then
+    if [[ $VERSION_TO_BUMP == "MINOR" ]]; then
         NEW_VERSION="$1.$(($2+1)).0"
-    elif [ $VERSION_TO_BUMP == "MAJOR" ]; then
+    elif [[ $VERSION_TO_BUMP == "MAJOR" ]]; then
         NEW_VERSION="$(($1+1)).0.0"
-    elif [ $VERSION_TO_BUMP == "PATCH" ]; then
+    elif [[ $VERSION_TO_BUMP == "PATCH" ]]; then
         NEW_VERSION="$1.$2.$(($3+1))"
     fi
 fi
