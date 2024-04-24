93c698bd463bd3a93badc0b5fede41de5ddcbc43
Abdulqadir Qasim <abdulqadir.qasim1@nhs.net>
2024-04-24 13:10:05 +0100

    Re-added logic to detect no tags in repo

--- scripts/pipeline/version.sh
+++ scripts/pipeline/version.sh
@@ -2,12 +2,19 @@
 
 set -x
 
-TAG_MESSAGE="New PR on $(date)"
+REPO_TAGS=$(git tag)
 SKIP_VERSION_BUMP=false
+TAG_MESSAGE="New PR on $(date)"
 VERSION=$(git describe --tags --abbrev=0)
 VERSION_TO_BUMP=$1
 
-if [ -z $VERSION ]; then
+if [[ -z $REPO_TAGS ]]; then
+    echo "No tags detected in repo. Adding initial tag '0.1.0'"
+    NEW_VERSION="0.1.0"
+    SKIP_VERSION_BUMP=true
+fi
+
+if [[ ("$SKIP_VERSION_BUMP" = false) && (-z $VERSION) ]]; then
     echo "Error: Unable to find previous Github Tag. Ensure at least the current closest tags is annotated."
     exit 1
 fi
