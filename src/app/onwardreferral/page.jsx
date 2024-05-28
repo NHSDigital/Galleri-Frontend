63d81f0470b428dbad45754c0e49bd75e31a60e2
avi312NHS <aviraj.mandair1@nhs.net>
2024-05-28 15:37:47 +0100

    added placeholer request for backend, fixed storing and checking of some cis2 info and added more roles for routing

--- src/app/onwardreferral/page.jsx
+++ src/app/onwardreferral/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "src/app/styles/css/sass.css";
 import { useSession } from "next-auth/react";
 import { useInactivity } from "@/app/context/AutoSignOutProvider";
@@ -8,9 +8,27 @@ import Header from "@/app/components/Header";
 import LoggedOut from "../views/logged_out/LoggedOut";
 
 export default function OnwardReferral() {
+  const CLINIC_INFORMATION = process.env.NEXT_PUBLIC_CLINIC_INFORMATION;
+
   const { data: session, status } = useSession({
     required: true,
   });
+  const [participants, setParticipants] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://${CLINIC_INFORMATION}.execute-api.eu-west-2.amazonaws.com/${ENVIRONMENT}/clinic-information?clinicId=${clinicId}&clinicName=${clinicName}`
+        );
+        const result = await response.json();
+        setParticipants(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const { showLogoutPage } = useInactivity();
 
@@ -23,6 +41,33 @@ export default function OnwardReferral() {
     return null;
   }
 
+  const participantsData = [
+    {
+      bloodDrawDate: "25/06/2024",
+      resultsReceived: "Yes",
+      participantName: "John Smith",
+      participantId: "1",
+    },
+    {
+      bloodDrawDate: "25/06/2024",
+      resultsReceived: "Yes",
+      participantName: "John Smith",
+      participantId: "2",
+    },
+    {
+      bloodDrawDate: "25/06/2024",
+      resultsReceived: "Yes",
+      participantName: "John Smith",
+      participantId: "3",
+    },
+    {
+      bloodDrawDate: "25/06/2024",
+      resultsReceived: "Yes",
+      participantName: "John Smith",
+      participantId: "4",
+    },
+  ];
+
   return !showLogoutPage ? (
     <>
       <Header withNavigation={true} />
@@ -30,7 +75,36 @@ export default function OnwardReferral() {
         <main className="nhsuk-main-wrapper" id="main-content" role="main">
           <div className="nhsuk-grid-row">
             <div className="nhsuk-grid-column-full">
-              <h1>Onward Referral</h1>
+              <h1>Onward referrals work list</h1>
+              <h2>Results table</h2>
+              <table
+                className="nhsuk-table-responsive nhsuk-u-margin-bottom-4"
+                role="table"
+              >
+                <thead role="rowgroup" className="nhsuk-table__head">
+                  <th role="row">Blood Draw Date</th>
+                  <th role="row">Results Received</th>
+                  <th role="row">Participant Name</th>
+                </thead>
+                <tbody>
+                  {participantsData.map((participant) => (
+                    <tr role="row">
+                      <>
+                        <td role="cell">{participant.bloodDrawDate}</td>
+                        <td role="cell">{participant.resultsReceived}</td>
+                        <td role="cell">
+                          <a
+                            href={`participant/${participant.participantId}`}
+                            role="link"
+                          >
+                            {participant.participantName}
+                          </a>
+                        </td>
+                      </>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </main>
