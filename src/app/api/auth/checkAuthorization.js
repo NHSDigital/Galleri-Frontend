d11e5a11fa67bbfe147f57c45996d2d4dbb14cb7
Amar-NHS <amar.rai5@nhs.net>
2024-06-03 16:35:59 +0100

    JSDoc-comments# added JsDoc comments

--- src/app/api/auth/checkAuthorization.js
+++ src/app/api/auth/checkAuthorization.js
@@ -1,12 +1,20 @@
-// Function to validate the Authorization check for CIS2 Users
-// returned from the Backend and also checks authorization for user
-// logged in via local auth
+/**
+ * Function to validate the Authorization check for Oauth/CIS2 Users
+ * returned from the Backend and also checks authorization for users
+ * logged in via local auth.
+ * @param {Object} user - The user object containing user details.
+ * @param {Object} account - The account object containing account details.
+ * @param {string} galleriActivityCode - The activity code required for authorization.
+ * @returns {Promise<boolean|string>} - Returns true if authorized, false if not authorized, or a string with the error URL if there's an issue.
+ */
+
 export async function checkAuthorization(user, account, galleriActivityCode) {
   // OAuth Sign In Check Result from Backend Authenticator
   if (account.type === "oauth") {
     return user?.isAuthorized;
-  } else {
-    // For Local auth below as no need to check all the Token claims as above
+  }
+  // For Local auth below as no need to check all the Token claims as above
+  else {
     if (!user.activityCodes.includes(galleriActivityCode)) {
       return "/autherror/activity_code_missing?error=Galleri+activity+code+missing+or+authentication+is+not+L3";
     }
@@ -18,7 +26,7 @@ export async function checkAuthorization(user, account, galleriActivityCode) {
       return "/autherror/account_not_found?error=User+Account+does+not+exist+or+is+inactive";
       // Keeping this here in case we want to route different users to different pages for referrals repo
     }
-    // Local Auth Checks Below
+    // Local Auth role check Below
     else if (
       user.role === "Invitation Planner" ||
       user.role === "Referring Clinician"
